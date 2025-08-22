c9bc287430a6bad7c6a8fe1c500ee7dce998fee3
AlbinaLapshina <72219652+AlbinaLapshina@users.noreply.github.com>
2025-08-22 16:20:31 +0000

    Первая задача

---
+++ pion/index.js
@@ -0,0 +1,9 @@
+function consoleLog (a) {
+    console.log("Новое сообщение 🍺: " + a)
+}
+
+let message;
+message = "Это сказала тебе Альбина"
+consoleLog(message)
+message = "Роман, любовь моя"
+consoleLog(message)
